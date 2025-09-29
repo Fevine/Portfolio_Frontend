90080a6f3b907fbc68849439873f1b210789ef4a
Albinoni <albinoni1423@gmail.com>
2025-09-29 14:30:58 +0400

    working on SideBar

---
+++ src/layouts/sideBar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./index.module.scss"
+
+function HomeSideBar() {
+  return (
+    <div className={style.sideBar}>
+
+    </div>
+  )
+}
+
+export default HomeSideBar
