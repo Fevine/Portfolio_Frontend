0b8cb93d7efdbb778da6d10d9fcd490fd4564172
Albinoni <albinoni1423@gmail.com>
2026-01-21 19:48:40 +0400

    ContactForm added, form seperated from contact page

---
+++ src/components/contactFrom/index.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import style from "./index.module.scss"
+
+function ContactForm() {
+
+  const [MessageData, setMessageData] = React.useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: ""
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setMessageData(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted:", MessageData);
+  };
+
+  return (
+    <>
+      <form className={style.contact} onSubmit={handleSubmit}>
+        <input
+          value={MessageData.name}
+          onChange={handleChange}
+          name="name"
+          className={style.name}
+          type="text"
+          placeholder='Name *'
+        />
+        <input
+          value={MessageData.email}
+          onChange={handleChange}
+          name="email"
+          className={style.email}
+          type="email"
+          placeholder='Email *'
+        />
+        <input
+          value={MessageData.phone}
+          onChange={handleChange}
+          name="phone"
+          className={style.phone}
+          type="tel"
+          placeholder='Phone'
+        />
+        <textarea
+          className={style.message}
+          value={MessageData.message}
+          onChange={handleChange}
+          name="message"
+          placeholder='Message *'
+          rows={7}
+        ></textarea>
+        <button
+          className={style.sendBtn}
+          type='submit'
+          disabled={
+            !MessageData.name.trim() ||
+            !MessageData.email.trim() ||
+            !MessageData.message.trim()
+          }
+        >
+          Send Message
+        </button>
+      </form>
+    </>
+  )
+}
+
+export default ContactForm
--- src/layouts/contact/index.jsx
+++ src/layouts/contact/index.jsx
@@ -1,74 +1,15 @@
 import React from 'react'
-import style from "./index.module.scss"
+import styles from "./index.module.scss"
+import ContactForm from '../../components/contactFrom';
 
 function Contact() {
 
-  const [MessageData, setMessageData] = React.useState({
-    name: "",
-    email: "",
-    phone: "",
-    message: ""
-  })
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setMessageData(prev => ({
-      ...prev,
-      [name]: value
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Submitted:", MessageData);
-  };
-
   return (
-    <form className={style.contact} onSubmit={handleSubmit}>
-      <input
-        value={MessageData.name}
-        onChange={handleChange}
-        name="name"
-        className={style.name}
-        type="text"
-        placeholder='Name *'
-      />
-      <input
-        value={MessageData.email}
-        onChange={handleChange}
-        name="email"
-        className={style.email}
-        type="email"
-        placeholder='Email *'
-      />
-      <input
-        value={MessageData.phone}
-        onChange={handleChange}
-        name="phone"
-        className={style.phone}
-        type="tel"
-        placeholder='Phone'
-      />
-      <textarea
-        className={style.message}
-        value={MessageData.message}
-        onChange={handleChange}
-        name="message"
-        placeholder='Message *'
-        rows={7}
-      ></textarea>
-      <button
-        className={style.sendBtn}
-        type='submit'
-        disabled={
-          !MessageData.name.trim() ||
-          !MessageData.email.trim() ||
-          !MessageData.message.trim()
-        }
-      >
-        Send Message
-      </button>
-    </form>
+    <>
+      <div className={styles.container}>
+        <ContactForm />
+      </div>
+    </>
   )
 }
 
