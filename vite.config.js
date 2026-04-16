c546ed6fdd363c69b3142446eb207bbd283f3525
Albinoni <albinoni1423@gmail.com>
2026-04-16 07:10:53 +0400

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env vars based on current mode (production, development, etc.)
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    define: {
+      // Explicitly define your env vars for the build
+      'import.meta.env.VITE_PUBLIC_KEY_EMAILJS': JSON.stringify(env.VITE_PUBLIC_KEY_EMAILJS),
+      'import.meta.env.VITE_SERVICE_ID_EMAILJS': JSON.stringify(env.VITE_SERVICE_ID_EMAILJS),
+      'import.meta.env.VITE_TEMPLATE_ID_EMAILJS': JSON.stringify(env.VITE_TEMPLATE_ID_EMAILJS),
+    }
+  }
 })
